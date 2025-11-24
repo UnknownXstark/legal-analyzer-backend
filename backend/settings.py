624848bd09cf4961a836d384dae13114e19b9400
UnknownXstark <nathanlouiskalio@gmail.com>
2025-11-24 16:20:44 +0100

    updates

--- backend/settings.py
+++ backend/settings.py
@@ -147,8 +147,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
+    'UNAUTHENTICATED_USER': None,
 }
 
 # Custom user model
