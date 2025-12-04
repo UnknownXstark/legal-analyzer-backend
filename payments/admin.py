07093ab84661abd53f970680ad5cf32ac63fb4c8
UnknownXstark <nathanlouiskalio@gmail.com>
2025-12-05 00:21:23 +0100

    updates

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Subscription
 # Register your models here.
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan", "active", "analysis_count", "billing_cycle_start", "current_period_end")
+    search_fields = ("user__username", "user__email", "stripe_customer_id", "stripe_subscription_id")
--- payments/admin_payments.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Subscription
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("user", "plan", "active", "analysis_count", "billing_cycle_start", "current_period_end")
-    search_fields = ("user__username", "user__email", "stripe_customer_id", "stripe_subscription_id")
